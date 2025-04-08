cb222637bd6dcab1230b57610a41dcb639874e9c
Milyor <millerarmando20@gmail.com>
2025-04-08 14:37:37 -0400

    Moved CommandLineRunner initDatabase Function to SecurityConfig

--- src/main/java/io/github/milyor/qrcodeservice/QrCodeServiceApplication.java
+++ src/main/java/io/github/milyor/qrcodeservice/QrCodeServiceApplication.java
@@ -1,15 +1,10 @@
 package io.github.milyor.qrcodeservice;
 
-import io.github.milyor.qrcodeservice.entity.Users;
-import io.github.milyor.qrcodeservice.repo.UserRepo;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Set;
 
 @SpringBootApplication
 @EnableCaching
@@ -19,21 +14,5 @@ public class QrCodeServiceApplication {
         SpringApplication.run(QrCodeServiceApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner initDatabase(UserRepo userRepo, PasswordEncoder passwordEncoder) {
-        return args -> {
-            String username ="user";
-            if (userRepo.findByUsername(username) != null) {
-                String encodedPassword = passwordEncoder.encode("password");
-                Set<String> authorities = Set.of("ROLE_USER", "ROLE_ADMIN");
-
-                Users newUser = new Users(username, encodedPassword, authorities);
-                userRepo.save(newUser);
-                System.out.println(">>> Created default user: " + username);
-            } else {
-                System.out.println(">>> Default " + username + " already exists");
-            }
-        };
-    }
 
 }
--- src/main/java/io/github/milyor/qrcodeservice/config/SecurityConfig.java
+++ src/main/java/io/github/milyor/qrcodeservice/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package io.github.milyor.qrcodeservice.config;
 
 
+import io.github.milyor.qrcodeservice.entity.Users;
+import io.github.milyor.qrcodeservice.repo.UserRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,6 +15,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +42,24 @@ public class SecurityConfig {
                     httpSecurityHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);
                 });
         return http.build();
+    }
+
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+        return args -> {
+            String username ="user";
+            if (userRepo.findByUsername(username) != null) {
+                String encodedPassword = passwordEncoder.encode("password");
+                Set<String> authorities = Set.of("ROLE_USER", "ROLE_ADMIN");
+
+                Users newUser = new Users(username, encodedPassword, authorities);
+                userRepo.save(newUser);
+                System.out.println(">>> Created default user: " + username);
+            } else {
+                System.out.println(">>> Default " + username + " already exists");
+            }
+        };
     }
 
 }
